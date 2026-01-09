689d6961f2430b5128cc2cb0829a5803f3909db6
Joshua Wehunt <joshuawehunt2@gmail.com>
2026-01-08 22:15:06 -0500

    Configure for static export (Netlify compatible)

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: ['images.unsplash.com', 'via.placeholder.com'],
+    unoptimized: true,
   },
 }
 
